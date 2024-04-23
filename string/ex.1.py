2e6c205e78ad48135a523f922ccc30c1f544a3b5
selvivirani <selvirani7@gmail.com>
2024-04-23 16:06:35 +0530

    tis is string

---
+++ string/ex.1.py
@@ -0,0 +1,12 @@
+"""
+    Exercise 1B: Create a string made of the middle three characters
+    Write a program to create a new string made of the middle three characters of an input string.
+
+"""
+str1="jenish"
+
+print(str1[2:4])
+
+str2="parth"
+
+print(str2[1:5])
---
+++ string/ex.2.py
@@ -0,0 +1,25 @@
+"""
+    Exercise 2: Append new string in the middle of a given string
+    Given two strings, s1 and s2. Write a program to create a new string s3 by appending s2 in the middle of s1.
+
+    Given:
+
+    s1 = "Ault"
+    s2 = "Kelly"
+    Expected Output:
+
+    AuKellylt
+
+"""
+
+s1="bhoomi"
+
+s2="kakdiya"
+
+s3=len(s1)
+
+mid= s3//2
+
+midum=s1[:mid] +s2 +s1[mid:]
+
+print(midum)
