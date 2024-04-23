e7fb15a6ac352721cfab3fb87ac2da928b611fcd
selvivirani <selvirani7@gmail.com>
2024-04-23 15:40:31 +0530

    add set

---
+++ set/Exercise_2.py
@@ -0,0 +1,19 @@
+"""
+    Exercise 2: Return a new set of identical items from two sets
+    Given:
+
+    set1 = {10, 20, 30, 40, 50}
+    set2 = {30, 40, 50, 60, 70}
+    Expected output:
+
+    {40, 50, 30}
+"""
+
+
+
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+
+new_set = set1.intersection(set2)
+
+print(new_set)
---
+++ set/Exercise_4.py
@@ -0,0 +1,20 @@
+"""
+    Exercise 4: Update the first set with items that don’t exist in the second set
+    Given two Python sets, write a Python program to update the first set with items that exist only in the first set and not in the second set.
+
+    Given:
+
+    set1 = {10, 20, 30}
+    set2 = {20, 40, 50}
+    Expected output:
+
+    set1 {10, 30}
+"""
+
+
+set1 = {10, 20, 30}
+set2 = {20, 40, 50}
+
+set1.difference_update(set2)
+
+print(set1)
