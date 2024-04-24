b6312c83a06e95b25780e589547933398cecbbe2
selvivirani <selvirani7@gmail.com>
2024-04-24 15:18:03 +0530

    add string folder

---
+++ string/ex.3.py
@@ -0,0 +1,14 @@
+"""
+    Exercise 3: Create a new string made of the first, middle, and last characters of each input string
+
+    Given two strings, s1 and s2, write a program to return a new string made of s1 and s2’s first, middle, and last characters.
+
+    Given:
+"""
+
+s1 = "kinal"
+s2 = "ramani"
+
+print(s1[0]+s2[0]+s1[3]+s2[5:])
+
+
---
+++ string/ex.4.py
@@ -0,0 +1,26 @@
+"""
+    Exercise 4: Arrange string characters such that lowercase letters should come first
+    Given string contains a combination of the lower and upper case letters. Write a program to arrange the characters of a string so that all lowercase letters should come first.
+
+    Given:
+
+    str1 = PyNaTive
+
+    Expected Output:
+
+    yaivePNT
+
+"""
+
+str= "python_basic"
+
+lower=[]
+upper=[]
+
+for char in str:
+    if char.islower():
+        lower.append(char)
+    else:
+        upper.append(char)
+str="".join(lower+upper)
+print(str)
